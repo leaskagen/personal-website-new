e3264bd0703d6c938c06b832cc88c01781db2638
leaskagen <50326406+leaskagen@users.noreply.github.com>
2022-10-27 18:07:40 +0200

    Update tab icon for dark and light mode

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -5,6 +5,35 @@
   </div>
 </template>
 
+<style>
+body {
+  background: linear-gradient(297deg, #ff6cbe, #f97e99, #dd98fd);
+    background-size: 600% 600%;
+
+    -webkit-animation: backgroundAnimation 59s ease infinite;
+    -moz-animation: backgroundAnimation 59s ease infinite;
+    animation: backgroundAnimation 59s ease infinite;
+}
+
+@-webkit-keyframes backgroundAnimation {
+    0%{background-position:0% 15%}
+    50%{background-position:100% 86%}
+    100%{background-position:0% 15%}
+}
+@-moz-keyframes backgroundAnimation {
+    0%{background-position:0% 15%}
+    50%{background-position:100% 86%}
+    100%{background-position:0% 15%}
+}
+@keyframes backgroundAnimation {
+    0%{background-position:0% 15%}
+    50%{background-position:100% 86%}
+    100%{background-position:0% 15%}
+}
+
+</style>
+
+
 <script>
 // @ is an alias to /src
 import HelloWorld from '@/components/HelloWorld.vue'
